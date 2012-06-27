9b47c37e28726fdf9b7d7542731df203c9ec0f92
Georges Toth <georges.toth@govcert.etat.lu>
2012-06-27 17:21:25 +0200

    fix license

--- rt_bot.py
+++ rt_bot.py
@@ -2,18 +2,18 @@
 
 # This file is part of rt_bot.
 #
-# Foobar is free software: you can redistribute it and/or modify
+# rt_bot is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Foobar is distributed in the hope that it will be useful,
+# rt_bot is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+# along with rt_bot.  If not, see <http://www.gnu.org/licenses/>.
 
 
 
